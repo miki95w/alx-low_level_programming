a83ae65234668d55ea8f49299c43d55427759d14
miki95w <mikiasworku87@gmail.com>
2022-04-13 15:12:31 -0700

    task 3

--- 0x02-functions_nested_loops/2-printt_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-printt_alphabet_x10.c
@@ -1,1 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+int count = 0;
+char letter;
+
+while (count++ <= 9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
 
