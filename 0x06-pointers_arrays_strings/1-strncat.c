f374fbad71ccf3621f0169fd1d98cebe0f563d27
miki95w <mikiasworku87@gmail.com>
2022-04-26 06:56:11 -0700

    Task12

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * *_strcat - concatenates two strings
- * @dest: second string to be concatenated to src
- * @src: first string to be concatenated to dest
+ * @dest: string to be concatenated
+ * @src: string to be concatenated until n limit
+ * @n: number of bytes that will be used from src
  * Return: pointer to the resukting string dest
  */
 char *_strncat(char *dest, char *src, int n)
