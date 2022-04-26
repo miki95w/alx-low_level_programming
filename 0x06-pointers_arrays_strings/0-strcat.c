6b8ee1f150793763625e2b646f5ac8cf737e2af1
miki95w <mikiasworku87@gmail.com>
2022-04-26 08:24:30 -0700

    c2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,6 +14,7 @@ int c, c2;
 c = 0;
 /*find the size of dest array*/
 while (dest[c])
+c++;
 /* iterate through each src array value without the null byte*/
 for (c2 = 0; src[c2] ; c2++)
 /*append src[c2] to dest[c] while overwritting the null byte in dest*/
